ac1ace8779562f99305db3d123d4c3b41ab36e41
choipureum <pooreumsunny@gmail.com>
2021-06-30 13:27:35 +0000

    setup

--- Procfile
+++
@@ -1,5 +0,0 @@
-web: /bin/true
-build: /bin/true
-seed: /bin/true
-migrate: /bin/true
-
---
+++ membership/src/test/java/com/example/membership/MembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
